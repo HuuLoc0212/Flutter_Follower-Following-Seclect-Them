019fca670b00ee966504a670c9752847b92ad345
HuuLoc0212 <nguynlc885@gmail.com>
2022-06-18 21:02:45 +0700

    updatet

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:day26/pages/following.dart';
+import 'package:day26/pages/select_theme.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: FollowingPage(),
+      debugShowCheckedModeBanner: false,
+    ));
